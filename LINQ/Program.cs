ec99988c36263e157d4790077ad5ef2dc31db433
forukotan <forukotan1@gmail.com>
2023-03-02 17:09:41 -0600

    video games 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>
+            { "halo","dragon quest","Final Fantasy","NBA2K","FIFA"};
+
+            var gamesOrdered = videoGames.OrderBy(x => x.Length);
+            foreach (var x in gamesOrdered)
+            {
+
+                Console.WriteLine(x);
+            }
         }
     }
 }
